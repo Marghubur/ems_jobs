0f646bbd71ccf0c895f73e46ea86151ef6df6be1
istiyakmi9 <istiyaq.mi9@gmail.com>
2024-01-28 11:48:09 +0530

    added jdbc

--- src/main/java/com/bot/jobs/JobsApplication.java
+++ src/main/java/com/bot/jobs/JobsApplication.java
@@ -1,4 +1,4 @@
-package com.bot.jobs.jobs;
+package com.bot.jobs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/bot/jobs/JobsApplicationTests.java
+++ src/test/java/com/bot/jobs/JobsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bot.jobs.jobs;
+package com.bot.jobs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
