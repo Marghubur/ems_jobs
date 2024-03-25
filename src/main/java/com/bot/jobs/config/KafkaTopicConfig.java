7c762cbb1376c025cebf8ff37e7cca75465dd035
istiyakmi9 <istiyaq.mi9@gmail.com>
2024-03-25 14:04:32 +0530

    added retention for message by time and size

--- src/main/java/com/bot/jobs/config/KafkaTopicConfig.java
+++ src/main/java/com/bot/jobs/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.bot.jobs.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -9,7 +10,12 @@ import org.springframework.kafka.config.TopicBuilder;
 public class KafkaTopicConfig {
     @Bean
     public NewTopic jobsManagerTopic() {
-        return TopicBuilder.name("daily-jobs-manager")
+        return TopicBuilder
+                .name("daily-jobs-manager")
+                .partitions(1)
+                .replicas(1)
+                .config(TopicConfig.RETENTION_MS_CONFIG, "86400000")
+                .config(TopicConfig.RETENTION_BYTES_CONFIG, "1073741824")
                 .build();
     }
 }
